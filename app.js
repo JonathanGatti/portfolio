db4468efed5627923b2a509b7ec0cfe91d405696
jonathan gatti <jonathangatti2@gmail.com>
2021-01-01 11:50:45 +0000

    work experience

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 const body = document.querySelector('body');
+const arrow = document.querySelector('.arrow');
 
 tl.to('.text', {y: '0%', duration: 1, stagger: 0.25})
-tl.to('.intro', {y: '-100%', duration: 1}, '-=1.5')
+tl.to('.intro', {y: '-100%', duration: 1})
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})
 tl.fromTo('#title', {opacity: 0}, {opacity: 1, duration: 1}, '-=1')
 
 document.addEventListener('scroll', () => {
-  tl.to('.project', {x: '10%', duration: 1.5, stagger: 0.25})
+  tl.to('.project', {x: '10%', duration: 1, stagger: 0.25})
 })
