9d172b749d8d5630ee80e46b926327a8576ac12e
jonathan gatti <jonathangatti2@gmail.com>
2021-01-05 12:28:46 +0000

    basic layout

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 const body = document.querySelector('body');
 const arrow = document.querySelector('.arrow');
+const header = document.querySelector('.header');
+const work = document.querySelector('.work-experience');
 
 tl.to('.text', {y: '0%', duration: 1, stagger: 0.25})
 tl.to('.intro', {y: '-100%', duration: 1})
 tl.fromTo('nav', {opacity: 0}, {opacity: 1, duration: 1})
-tl.fromTo('#title', {opacity: 0}, {opacity: 1, duration: 1}, '-=1')
+tl.fromTo('.header', {opacity: 0}, {opacity: 1, duration: 1}, '-=1')
 
-document.addEventListener('scroll', () => {
-  tl.to('.project', {x: '10%', duration: 1, stagger: 0.25})
-})
+// window.addEventListener('scroll', function() {
+//   document.querySelector('.header').innerHTML = window.pageYOffset + 'px';
+// });
+
+
+
