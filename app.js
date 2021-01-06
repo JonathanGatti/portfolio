97739f7ec130209ea8de8999a9e5a4ed8cb9afa3
jonathan gatti <jonathangatti2@gmail.com>
2021-01-06 11:46:49 +0000

    button eventListeners

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const tl = gsap.timeline({defaults: {ease: 'power1.out'}});
 const body = document.querySelector('body');
-const arrow = document.querySelector('.arrow');
 const header = document.querySelector('.header');
 const work = document.querySelector('.work-experience');
+const button = document.querySelector('.about-button');
 
 tl.to('.text', {y: '0%', duration: 1, stagger: 0.25})
 tl.to('.intro', {y: '-100%', duration: 1})
@@ -12,6 +12,18 @@ tl.fromTo('.header', {opacity: 0}, {opacity: 1, duration: 1}, '-=1')
 // window.addEventListener('scroll', function() {
 //   document.querySelector('.header').innerHTML = window.pageYOffset + 'px';
 // });
+
+button.addEventListener('mouseenter', () => {
+  document.querySelector('i').className='fas fa-arrow-down';
+})
+
+button.addEventListener('mouseleave', () => {
+  document.querySelector('i').className='fas fa-arrow-right';
+})
+
+button.addEventListener('click', () =>{
+  window.scrollBy(0,750)
+})
 
 
 
