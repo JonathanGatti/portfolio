df0d1cee12e36572165c6589fa6de8b04234c3d4
jonathan gatti <jonathangatti2@gmail.com>
2021-01-10 15:19:06 +0000

    changed imgs and refacotred

--- modules/projects.js
+++ modules/projects.js
@@ -1,17 +1,15 @@
-import {tl} from './timeline.js';
-
 const projects = [
   {
     title: 'The Football App',
-    imgSrc: 'https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vdGJhbGx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    imgSrc: 'https://images.unsplash.com/photo-1529900748604-07564a03e7a6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MjJ8fGZvb3RiYWxsfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Twitch Clone',
-    imgSrc: 'https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vdGJhbGx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    imgSrc: 'https://images.unsplash.com/photo-1527334919515-b8dee906a34b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   },
   {
     title: 'Web Framework',
-    imgSrc: 'https://images.unsplash.com/photo-1508098682722-e99c43a406b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vdGJhbGx8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+    imgSrc: 'https://images.unsplash.com/photo-1553964125-e03d833522b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fHdlYiUyMGZyYW1ld29ya3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
   },
 ]
 
@@ -30,19 +28,25 @@ const renderProjects = () => {
     div.append(title, img);
     container.appendChild(div)
 
-    img.addEventListener('mouseenter', () => {
-      title.style.transform = 'translateY(150px)'
-      title.style.opacity = 1
-    })
-    img.addEventListener('mouseleave', () => {
-      title.style.transform = 'translateY(0)';
-      title.style.opacity = 0;
-    })
+    img.addEventListener('mouseenter', () => onMouseEnter(title, img))
+    img.addEventListener('mouseleave', () => onMouseLeave(title, img))
   })
 
   return container;
 }
 
+//img styles
+const onMouseEnter = (title, img) => {
+  title.style.transform = 'translateY(150px)'
+  title.style.opacity = 1
+  img.style.opacity = 0;
+}
+
+const onMouseLeave = (title, img) => {
+  title.style.transform = 'translateY(0)';
+  title.style.opacity = 0;
+  img.style.opacity = 1;
+} 
 
 
 export {renderProjects};
