b9bcd0fff52ea08ed3e672f627a5e8810888d787
jonathan gatti <jonathangatti2@gmail.com>
2021-01-13 11:04:35 +0000

    added description

--- modules/work.js
+++ modules/work.js
@@ -9,10 +9,27 @@ const renderWorkExperience = () => {
     title.innerHTML = work.employer;
     let subTitle = document.createElement('h5');
     subTitle.innerHTML = work.position;
+    subTitle.style.color = 'rgb(50, 169, 151)'
     div.append(title, subTitle)
     container.append(div)
+
+    div.addEventListener('mouseenter', () => onMouseEnter(div, work))
+    div.addEventListener('mouseleave', () => onMouseLeave())
   })
   return container;
+}
+
+//work project animations 
+const onMouseEnter = (div, work) => {
+  let description = document.createElement('p');
+  description.innerHTML = work.description;
+  description.className = 'description';
+  div.appendChild(description);
+}
+
+const onMouseLeave = () => {
+  let description = document.querySelector('.description');
+  description.remove()
 }
 
 export {renderWorkExperience};
--- utils/work.js
+++ utils/work.js
@@ -2,11 +2,13 @@ export const workExperience = [
   {
     employer: 'Brains & Brawn Studio',
     position: 'Junior React Developer',
-    description: ''
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+    dates: 'october 2020 / present'
   },
   {
     employer: 'Jayne Selvaggio',
     position: 'React Developer',
-    description: ''
+    description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry',
+    dates: 'september 2020 / october 2020'
   }
 ]
