438d67d8730d0bacf14d9c44b37f397a6e4c33be
jonathan gatti <jonathangatti2@gmail.com>
2021-01-13 14:41:18 +0000

    changed projects

--- utils/projects.js
+++ utils/projects.js
@@ -7,17 +7,17 @@ export const javascript = [
     tech: 'React.js/TypeScript' 
   },
   {
-    title: 'Twitch Clone',
-    imgSrc: 'https://images.unsplash.com/photo-1527334919515-b8dee906a34b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
-    href: 'https://jonathangatti.github.io/football-app-v3/#/',
-    codeUrl: 'https://github.com/JonathanGatti/football-app-v3',
+    title: 'Color App',
+    imgSrc:'https://images.unsplash.com/photo-1525909002-1b05e0c869d8?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=675&q=80',
+    href: 'https://jonathangatti.github.io/color-app/',
+    codeUrl: 'https://github.com/JonathanGatti/color-app',
     tech: 'React.js/TypeScript' 
   },
   {
     title: 'Web Framework',
     imgSrc: 'https://images.unsplash.com/photo-1553964125-e03d833522b2?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mzh8fHdlYiUyMGZyYW1ld29ya3xlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60',
-    href: 'https://jonathangatti.github.io/football-app-v3/#/',
-    codeUrl: 'https://github.com/JonathanGatti/football-app-v3',
+    href: 'https://github.com/JonathanGatti/web-framework',
+    codeUrl: 'https://github.com/JonathanGatti/web-framework',
     tech: 'TypeScript' 
   },
 ]
