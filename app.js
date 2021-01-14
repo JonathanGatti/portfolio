2bf57633f54b22b8fe5f7aa923834885c931b392
jonathan gatti <jonathangatti2@gmail.com>
2021-01-14 10:24:44 +0000

    responsive navbar

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ import {renderProjects} from './modules/projects.js';
 
 const button = document.querySelector('.about-button');
 const header = document.querySelector('.header-text');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('mouseenter', () => {
+  hamburger.style.transform= 'rotate(90deg)'
+  navLinks.classList.add('show');
+  navLinks.style.transform = 'translate(-30px, 25px)'
+})
+
+navLinks.addEventListener('mouseenter', () => {
+  navLinks.classList.add('show');
+})
+
+navLinks.addEventListener('mouseleave', () => {
+  navLinks.classList.remove('show');
+})
+
+hamburger.addEventListener('mouseleave', () => {
+  hamburger.style.transform= 'rotate(0deg)'
+  navLinks.classList.remove('show')
+})
 
 const headerAnimation = () => {
   header.style.opacity = 1;
